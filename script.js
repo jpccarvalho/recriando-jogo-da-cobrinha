fc4e43936e4d3a9259638fc91115e0c468f6b1eb
João Paulo Carvalho <9712239j@gmail.com>
2021-05-03 11:06:38 -0300

    Adicionando a lógica inicial de movimento

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 
+let direction = "right";
+
+
+
 snake[0] = {
     x: 8 * box,
     y: 8* box
 }
+
 
 
 function criarBG(){
@@ -22,6 +27,27 @@ function criarCobrinha(){
     }
 }
 
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
 
-criarBG();
-criarCobrinha();
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = { 
+        x:snakeX, 
+        y:snakeY
+    }
+    
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo(), 100);
+
